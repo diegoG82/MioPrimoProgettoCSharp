dd6d9e5f238f5823e91bfcca12c4919de6336175
diegoG82 <diegogiardina@hotmail.it>
2023-09-05 13:00:44 +0200

    Aggiungere i file di progetto.

---
+++ MioPrimoProgettoCSharp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ciao utente!");
+Console.WriteLine("Come ti chiami?");
+
+var nome = Console.ReadLine();
+
+Console.WriteLine("Benvenuto " +  nome  + ", piacere di conoscerti");
